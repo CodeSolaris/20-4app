375a64de6e10f71833875451f9b50cce2d8834fe
CodeSolaris <aleksandr.volkov.it@gmail.com>
2023-11-13 00:24:05 +0200

    redesign pdf file

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import pandas as pd
 from fpdf import FPDF
 
 INVOICE_DIRECTORY = "./invoices/"
-CELL_WIDTH = 35
+CELL_WIDTH = 37
 CELL_HEIGHT = 12
 
 
@@ -17,48 +17,47 @@ def format_date(date_string):
 
 invoice_files_path = glob.glob(f"{INVOICE_DIRECTORY}*.xlsx")
 
-for invoice_file_path in invoice_files_path:
+for file_path in invoice_files_path:
     pdf = FPDF(orientation="P", unit="mm", format="A4")
 
     pdf.add_page()
 
-    invoice_file_name = Path(invoice_file_path).stem
+    invoice_file_name = Path(file_path).stem
     invoice_num, date = invoice_file_name.split("-")
     formatted_date = format_date(date)
 
-    df = pd.read_excel(Path(invoice_file_path))
+    df = pd.read_excel(Path(file_path))
 
     pdf.set_auto_page_break(auto=False, margin=10)
-    pdf.set_font("Times", size=8, style="B")
-    pdf.set_text_color(150, 150, 150)
+    pdf.set_font("Times", size=14, style="B")
 
-    pdf.cell(160, 10, txt=f"Invoice Number: {invoice_num}", ln=0, align="L")
-    pdf.cell(0, 10, txt=formatted_date, ln=0, align="L")
+    pdf.cell(0, CELL_HEIGHT, txt=f"Invoice Number: {invoice_num}", ln=1, align="L")
+    pdf.cell(0, CELL_HEIGHT, txt=f"Date: {formatted_date}", ln=1, align="L")
+    pdf.ln()
 
-    pdf.set_text_color(0, 0, 0)
-    pdf.line(10, 18, 200, 18)
-    pdf.ln(20)
+    pdf.set_left_margin(10)
 
-    pdf.set_left_margin(20)
-    pdf.set_font("Times", size=10, style="B")
-    pdf.cell(CELL_WIDTH, CELL_HEIGHT, txt=df.columns[0], ln=0, border=1, align="C")
-    pdf.cell(CELL_WIDTH, CELL_HEIGHT, txt=df.columns[1], ln=0, border=1, align="C")
-    pdf.cell(CELL_WIDTH, CELL_HEIGHT, txt=df.columns[2], ln=0, border=1, align="C")
-    pdf.cell(CELL_WIDTH, CELL_HEIGHT, txt=df.columns[3], ln=0, border=1, align="C")
-    pdf.cell(CELL_WIDTH, CELL_HEIGHT, txt=df.columns[4], ln=1, border=1, align="C")
+    pdf.set_font("Times", size=12, style="B")
+    for column_name in df.columns:
+        column_name = column_name.replace("_", " ").capitalize()
+        pdf.cell(CELL_WIDTH, CELL_HEIGHT, txt=str(column_name), border=1, align="C")
+    pdf.ln()
+    pdf.set_font("Times", size=8)
+
 
     for index, row in df.iterrows():
-        pdf.set_font("Times", size=8)
-        pdf.cell(CELL_WIDTH, 10, txt=str(row["product_id"]), ln=0, border=1, align="C")
-        pdf.cell(
-            CELL_WIDTH, 10, txt=str(row["product_name"]), ln=0, border=1, align="C"
-        )
-        pdf.cell(
-            CELL_WIDTH, 10, txt=str(row["amount_purchased"]), ln=0, align="C", border=1
-        )
-        pdf.cell(
-            CELL_WIDTH, 10, txt=str(row["price_per_unit"]), ln=0, align="C", border=1
-        )
-        pdf.cell(CELL_WIDTH, 10, txt=str(row["total_price"]), ln=1, align="C", border=1)
+        for column in df.columns:
+            value = row[column]
+            pdf.cell(CELL_WIDTH, CELL_HEIGHT, txt=str(value), border=1, align="C")
+        pdf.ln()  
+    cell_width = CELL_WIDTH*4
+    total_amount = df["total_price"].sum()
+    pdf.cell(cell_width, CELL_HEIGHT, txt="", ln=0, border=1)
+    pdf.cell(CELL_WIDTH, CELL_HEIGHT, txt=f"{total_amount}", ln=1, border=1, align="C")
+    pdf.ln()
 
+    pdf.set_font("Times", size=12, style="B")
+    pdf.cell(0, CELL_HEIGHT, txt=f"Total Amount: {total_amount}", ln=1, align="L")
+    pdf.cell(70, CELL_HEIGHT, txt="Thank you for your business!", ln=0, align="L")
+    pdf.image("invoice_logo.png", w=12)
     pdf.output(f"{INVOICE_DIRECTORY}{invoice_num}.pdf")
